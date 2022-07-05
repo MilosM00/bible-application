56665b5da0cced5d2cd3553ab5088e9efbe571ff
Milos Milijasevic <milosrm20005@gmail.com>
2022-07-05 20:20:04 +0200

    navbar-js updated

---
+++ src/components/Navbar.js
@@ -0,0 +1,34 @@
+import React from "react";
+import "./Navbar.css";
+
+const Navbar = (props) =>{
+    return(
+
+        <div>
+        
+            <div className="navbar">
+
+                <p className="hamburger" onClick={props.listEvent}>
+                    {
+                        props.showList === true ? <i className="fa fa-bars"></i> : <i className="fa fa-times"></i>
+                    }
+                </p>
+
+            </div>
+
+            {props.showList === false &&
+                <div className="navbar-list">
+                    <ul>
+                        <li><a href="https://github.com/">Home</a></li>
+                        <li><a href="https://github.com/">About</a></li>
+                        <li><a href="https://github.com/">Find</a></li>
+                    </ul>
+                </div>
+            }
+
+        </div>
+
+    );
+};
+
+export default Navbar;

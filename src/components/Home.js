97fac5f8ada02938e05e85b1835fe0be239bdb52
Milos Milijasevic <milosrm20005@gmail.com>
2022-07-06 10:12:45 +0200

    home-js updated

---
+++ src/components/Home.js
@@ -0,0 +1,23 @@
+import React from "react";
+import "./Home.css";
+
+const Home = () =>{
+    return(
+
+        <div className="home">
+            <p className="home-welcome">Welcome!</p>
+            <p className="home-nice">Nice to have you here!</p>
+            <p className="home-application">This is a bible application</p>
+            <p className="home-story">A short story and explanation</p>
+
+            <div className="inline-home">
+                <a href="https://www.bible.com/" target="_blank" rel="noreferrer">Start Reading Now</a>
+                <a href="https://github.com/">Explore More</a>
+            </div>
+
+        </div>
+
+    );
+};
+
+export default Home;

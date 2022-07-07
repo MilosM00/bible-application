a720ad5e6de07d7f0eb3c53e382b2dbf99cfdf1b
Milos Milijasevic <milosrm20005@gmail.com>
2022-07-07 10:51:10 +0200

    navbar-js updated

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,11 +8,19 @@ const Navbar = (props) =>{
         
             <div className="navbar">
 
+                <p className="navbar-title">Bible Application</p>
+
                 <p className="hamburger" onClick={props.listEvent}>
                     {
                         props.showList === true ? <i className="fa fa-bars"></i> : <i className="fa fa-times"></i>
                     }
                 </p>
+
+                <ul>
+                    <li><a href="https://github.com/">Home</a></li>
+                    <li><a href="https://github.com/">About</a></li>
+                    <li><a href="https://github.com/">Find</a></li>
+                </ul>
 
             </div>
 

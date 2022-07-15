35e5eaa4d8dc029c965d4c6820c5d58d1a4baeec
Milos Milijasevic <milosrm20005@gmail.com>
2022-07-15 11:14:20 +0200

    app-js updated

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
 import About from "./components/About";
+import Chapter from "./components/Chapter";
 
 const App = () =>{
 
@@ -20,6 +21,42 @@ const App = () =>{
         document.body.scrollIntoView();
     }
 
+    const [dataApi, setDataApi] = React.useState("");
+
+    const [newVerse, setNewVerse] = React.useState(true)
+    const [book, setBook] = React.useState("genesis");
+    const [chapter, setChapter] = React.useState("6");
+    const [verse, setVerse] = React.useState("14");
+
+    React.useEffect(() =>{
+
+        const findChapter = async () =>{
+
+            const response = await fetch(`https://bible-api.com/${book}${chapter}:${verse}`);
+            const data = await response.json();
+            setDataApi(data);
+
+        };
+        findChapter();
+
+    }, [newVerse]); // eslint-disable-line react-hooks/exhaustive-deps
+
+    const bookOnChange = (e) =>{
+        setBook(e.target.value);
+    };
+
+    const chapterOnChange = (e) =>{
+        setChapter(e.target.value);
+    };
+
+    const verseOnChange = (e) =>{
+        setVerse(e.target.value);
+    };
+
+    const findEvent = () =>{
+        setNewVerse(!newVerse);
+    };
+
     return(
 
         <div>
@@ -34,6 +71,17 @@ const App = () =>{
             <Home />
 
             <About />
+
+            <Chapter 
+                dataApi={dataApi.text}
+                findEvent={findEvent}
+                book={book}
+                chapter={chapter}
+                verse={verse}
+                bookOnChange={bookOnChange}
+                chapterOnChange={chapterOnChange}
+                verseOnChange={verseOnChange}
+            />
 
         </div>
     );

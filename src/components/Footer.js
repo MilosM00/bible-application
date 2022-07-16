61e347973bf32039adae7e5a92035b08a1db8cae
Milos Milijasevic <milosrm20005@gmail.com>
2022-07-16 21:45:24 +0200

    footer-js updated

---
+++ src/components/Footer.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./Footer.css";
+
+const Footer = () =>{
+    return(
+
+        <div className="footer">
+
+            <p className="info-footer">
+                Designed and Developed by <a href="https://github.com/MilosM00" target="_blank" rel="noreferrer">MM</a>.
+            </p>
+
+        </div>
+
+    );
+};
+
+export default Footer;
